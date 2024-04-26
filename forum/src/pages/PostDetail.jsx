12e1c2d3cd0422c8c9c2bea7dcb32d1ebeeff2d4
An <pyop.van.an@gmail.com>
2024-04-26 19:48:06 -0400

    post detail page

--- forum/src/pages/PostDetail.jsx
+++ forum/src/pages/PostDetail.jsx
@@ -11,7 +11,7 @@ const PostDetail = () => {
         const fetchPost = async () => {
             const { data } = await supabase
                 .from("forum")
-                .select("id, question, content, imageURL")
+                .select("id, created_at, question, content, imageURL")
                 .eq("id", id)
                 .single()
 
@@ -22,18 +22,36 @@ const PostDetail = () => {
     }, [id])
 
 
-    const getPost = () => {
+    function timeAgo(dateString) {
+        const now = new Date();
+        const past = new Date(dateString);
+        const secondsPast = (now.getTime() - past.getTime()) / 1000;
 
+        if (secondsPast < 3600) {
+            return parseInt(secondsPast / 60) + ' minutes';
+        }
+        if (secondsPast <= 86400) {
+            return parseInt(secondsPast / 3600) + ' hours';
+        }
+        if (secondsPast <= 604800) {
+            return parseInt(secondsPast / 86400) + ' days';
+        }
+        if (secondsPast <= 2629800) {
+            return parseInt(secondsPast / 604800) + ' weeks';
+        }
+        if (secondsPast > 2629800) {
+            return parseInt(secondsPast / 2629800) + ' months';
+        }
     }
 
     return (
         <div className='post-detail'>
             <div className='post-detail-card'>
-                <h1>{post.question}</h1>
-                <h1>{post.content}</h1>
-                <h1>{post.imageURL}</h1>
+                <h1 className='post-detail-time'>Posted {timeAgo(post.created_at)} ago</h1>
+                <h1 className='post-detail-question'>{post.question}</h1>
+                <h1 className='post-detail-content'>{post.content}</h1>
+                <h1 className='post-detail-imageURL'>{post.imageURL}</h1>
             </div>
-
         </div>
     )
 }
